f3557013e1d39faf91beb3d95272f04e02253d9f
Ftardi <Roman.saharoza@yandex.ru>
2026-04-14 14:39:33 +0300

    Изменена приветственная строка для задания по Git

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Привет! Это изменение из Visual Studio для задания по Git");
